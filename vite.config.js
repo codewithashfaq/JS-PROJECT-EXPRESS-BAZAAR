60badc4cc876698408403430a330da5c7aa7daa2
Ashfaq Khan <ashfaaq.alam@outlook.com>
2026-05-10 22:56:50 +0530

    Fix vite config for multi-page build

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
-import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        addToCart: resolve(__dirname, "addToCart.html"),
-        signIn: resolve(__dirname, "signIn.html"),
-        signUp: resolve(__dirname, "signUp.html"),
+        main: "index.html",
+        addToCart: "addToCart.html",
+        signIn: "signIn.html",
+        signUp: "signUp.html",
       },
     },
   },
